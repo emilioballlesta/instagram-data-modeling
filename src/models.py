1b2a3e6b22775eb99d67ac950234abd49c96ae08
Emilio_Ballesta <efballes@uc.cl>
2020-05-25 22:01:56 -0400

    first commit

--- src/models.py
+++ src/models.py
@@ -14,20 +14,63 @@ class Person(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(), nullable=False)
+    photo = Column(String) #Because it's a HTML Structure at the end
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
+    photo = Column(String()) #Because it's a HTML structure at the end
+    date = Column(String())
+    number_of_likes = Column(Integer)
+    number_of_comments = Column(Integer)
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
 
-    def to_dict(self):
-        return {}
+class Story(Base):
+    __tablename__ = 'story'
+    id = Column(Integer, primary_key=True)
+    story_video = Column(String()) #Because it's a HTML structure at the end
+    date = Column(String()) #Actually it's a date but I can't put it as a date without an error
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
 
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = id = Column(Integer, primary_key=True)
+    content = Column(String())
+    person_id = Column(Integer, ForeignKey('person.id'))
+    date = Column(String())
+    person = relationship(Person)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+
+class Like(Base):
+    __tablename__ = 'like'
+    id = id = Column(Integer, primary_key=True)
+    like = Column(String()) #Actually it's a Boolean, but I can't put it as a boolean without an error
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+    comment_id = Column(Integer, ForeignKey('comment.id'))
+    comment = relationship(Comment)
+
+class Chat(Base):
+    __tablename__ = 'chat'
+    id = id = Column(Integer, primary_key=True)
+    messages = Column(Integer)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
+
+class Message(Base):
+    __tablename__ = 'messages'
+    id = id = Column(Integer, primary_key=True)
+    content = Column(String())
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
+    chat_id = Column(Integer, ForeignKey('chat.id'))
+    chat = relationship(Chat)
+    
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
